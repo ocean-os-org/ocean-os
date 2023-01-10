1d12fe04555224f106b8a753ddcdec0638da45ef
Rui Gil <rui@gil.com>
2023-01-10 17:35:07 +0000

    pwa config

--- packages/oos-local-webui/vite.config.ts
+++ packages/oos-local-webui/vite.config.ts
@@ -6,6 +6,33 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'prompt' })
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['/assets/icons/favicon.ico', '/assets/icons/apple-touch-icon.png', '/assets/icons/safari-pinned-tab.svg'],
+      manifest: {
+        name: 'OceanOS',
+        short_name: 'OceanOS',
+        description: 'OceanOS Personal Operating System',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }          
+        ]
+      }
+    })
   ],
 })
