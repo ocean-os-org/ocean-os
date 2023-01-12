2082750ea15ae664ea29860e0ba9932f49c7d5d5
Rui Gil <rui@gil.com>
2023-01-12 01:06:15 +0100

    rollup options

--- packages/oos-local-webui/vite.config.ts
+++ packages/oos-local-webui/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   build: {
     rollupOptions: {
